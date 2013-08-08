409a55f8529019691a42d521c38c67be340d0349
ondrej.uzovic@gmail.com <ondrej.uzovic@gmail.com@db685abb-55d0-fa71-5497-1d9f13bdcf98>
2013-08-08 16:44:17 +0000

    COM GUID removed from the assembly info.

--- EneterProtoBufSerializer/EneterProtoBufSerializer/Properties/AssemblyInfo.cs
+++ EneterProtoBufSerializer/EneterProtoBufSerializer/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eneter")]
 [assembly: AssemblyProduct("Eneter.ProtoBufSerializer")]
-[assembly: AssemblyCopyright("Ondrej Uzovic")]
+[assembly: AssemblyCopyright("Copyright © Ondrej Uzovic 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +19,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fb4d708b-a4ef-4b5c-9dc2-f400883cf496")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,3 +29,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
